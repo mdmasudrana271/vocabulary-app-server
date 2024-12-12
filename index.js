5f3d8959447f07087970b48413dd1c8c68348b67
mdmasudrana271 <mr7123619@gmail.com>
2024-12-13 02:08:21 +0600

    modify a function

--- index.js
+++ index.js
@@ -106,7 +106,7 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/users/admin/:email", async (req, res) => {
+    app.get("/admin/:email", async (req, res) => {
       const email = req.params.email;
       const query = { email };
       const user = await usersCollection.findOne(query);
